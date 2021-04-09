05759f4a647985637cbb781ad4213c0d9d128efc
SherazLiaqat <sherazliaqat099@gmailcom>
2021-04-09 16:45:13 +0500

    Home page Dropdown

--- react-2/src/components/Herosection/Herosection.js
+++ react-2/src/components/Herosection/Herosection.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React,{useState} from 'react'
 //import '../../App.css';
 import './Herosection.css';
-import {  IoMdBulb} from 'react-icons/io';
+import { IoMdArrowDropdown} from 'react-icons/io';
 import homeimage from '../../components/images/img-12.jpg';
+import { Link } from 'react-router-dom';
+import Dropdowns from '../Navbar/Dropdown';
+
+
 function Herosection() {
+  
+  const [dropdowns, setDropdowns] = useState(false);
+  const onMouseEnters = () => {
+    if (window.innerWidth < 960) {
+      setDropdowns(false);
+    } else {
+      setDropdowns(true);
+    }
+  };
+
+  const onMouseLeaves = () => {
+    if (window.innerWidth < 960) {
+      setDropdowns(false);
+    } else {
+      setDropdowns(false);
+    }
+  };
     return (
         <div className='hero-container'>
         <img className='image' src={homeimage}/>
@@ -11,10 +32,21 @@ function Herosection() {
         <p>What are you waiting for?</p>
         
         <div className='hero-btns'>
+          <Link
+          to='/Login'>
         <button className='Button-home'> GET STARTED</button>
-         
-          <button className='Button-home2'>ESTIMATION<IoMdBulb/> </button>
-         
+        
+        </Link>
+        <li 
+            onMouseEnter={onMouseEnters}
+            onMouseLeave={onMouseLeaves}
+            >
+               
+          <button className='Button-home2'>ESTIMATION< IoMdArrowDropdown /> 
+          
+          </button>
+          {dropdowns && <Dropdowns />}
+          </li>
         </div>
       </div>
     );
--- react-2/src/components/My-Blog/Blog.js
+++ react-2/src/components/My-Blog/Blog.js
@@ -1,36 +1,50 @@
 import React,{useState} from 'react'
 import Hero from './Hero'
-
+import Paper from '@material-ui/core/Paper';
+import { Container } from '@material-ui/core';
 const Blog = () => {
+const [quotes,setquotes]=useState([]);
+const [textvalue,settextvalue]=useState("");
+const [open,setopen]=useState(false);
+   
+const onChangeHandler=(e)=>{
+       settextvalue(e,textvalue);
+   }
+   const onsubmitHandler=(e,textvalue)=>{
+    
+    if(textvalue===""){
 
-    const openSearch = () => {
-        setSearch(true);
+    }else{
+const newquote={
+    id:Math.floor(Math.random()*1000),
+    deleted:false,
+    description:textvalue
+};
+const newquotes=[...quotes,newquotes];
+setquotes(newquotes);
     }
-    const [search, setSearch] = useState(false);
-
-
-    const submitSearch = (e) => {
-        e.preventDefault();
-        alert('Searhed');
-
-    }
+};
     return (
         <div>
            <h1>This is blog page</h1> 
-           <div >
-            <form onSubmit={submitSearch}>
-                <input type="text"  placeholder="Search" />
-                <img onClick={openSearch} className="searchIcon"  alt="Search" />
-            </form>
-            <div className='container1'>
-                <h1>This is 1 div</h1>
-            </div>
-            <div className='container2'>
-                <h1>This is 2 div</h1>
-            </div>
-        </div>
-       
-           <Hero/>
+           <input
+          className=''
+          type='text'
+          name='username'
+          placeholder='Add quotes'
+          onChange={onChangeHandler}
+          />
+          <button className='SignupButton' onSubmit={onsubmitHandler}>Add Quote</button>
+          <Container>
+             {quotes.map((quote)=>(
+
+             
+
+<Paper>
+    <p>{quote.description}</p>
+</Paper>
+))}
+ </Container>
         </div>
     )
 }
