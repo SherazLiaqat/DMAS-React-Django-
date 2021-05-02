6eec064f4b846968e586e1a3c6576b9e4ad33ccc
SherazLiaqat <sherazliaqat099@gmailcom>
2021-05-02 12:56:30 +0500

    new news

--- react-2/src/components/News/News.js
+++ react-2/src/components/News/News.js
@@ -11,8 +11,11 @@ export default class News extends React.Component {
     const url = "https://api.reliefweb.int/v1/disasters?appname=rwint-user-0&profile=full&preset=latest&slim=1";
     const response = await fetch(url);
     const data = await response.json();
+    
+    
 
-    this.setState(  {  person: data.data[0], loading: false });
+    this.setState(  {  person: data.data[7], loading: false });
+    
   }
 
   render() {
@@ -26,10 +29,16 @@ export default class News extends React.Component {
 
     return (
       <div>
-        <div>{this.state.person.id}</div>
-        <div>{this.state.person.score}</div>
-        <div>{this.state.person.fields.date.created}</div>
-        <div>{this.state.person.fields.primary_type.name}</div>
+        <div><h2>ID</h2>{this.state.person.id}</div>
+        <div><h2>SCORE</h2>{this.state.person.score}</div>
+        <div><h2>STATUS</h2>{this.state.person.fields.status}</div>
+        <div><h2>DATE</h2>{this.state.person.fields.date.created}</div>
+        <div><h2>TYPE</h2>{this.state.person.fields.primary_type.name}</div>
+        <div><h2>HEADLINE</h2>{this.state.person.fields.name}</div>
+        
+        <div><h2>NAME</h2>{this.state.person.fields.country[0].name}</div>
+        <div><h2>LOCATION</h2>{this.state.person.fields.country[0].location.lat}</div>
+        <div><h2>DESCRIPTION</h2>{this.state.person.fields.description}</div>
       </div>
     );
   }
--- react-2/src/components/News/Newsuser.js
+++ react-2/src/components/News/Newsuser.js
@@ -13,14 +13,14 @@ const githubUsers = ({users}) => {
 
                         users.map((curElem) => {
                         
-                            const { avatar_url, id, login, type } = curElem;
+                            const { avatar_url, id, fields, type } = curElem;
                         return (
                               <div className="col-10 col-md-4 mt-5" key={id}>
                       <div className="card p-2">
                                 <div className="d-flex align-items-center">
-                                        <div className="image"> <img src={ avatar_url } className="rounded" width="155" /> </div>
+                                        <div className="image"> <img src=" " className="rounded" width="155" /> </div>
                                     <div className="ml-3 w-100">
-                                            <h4 className="mb-0 mt-0 textLeft">{login} <FcApproval/> </h4>
+                                            <h4 className="mb-0 mt-0 textLeft">{fields} <FcApproval/> </h4>
                                             {/* <span className="text-left">{type }</span> */}
                                         <div className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
                                                 <div className="d-flex flex-column">
