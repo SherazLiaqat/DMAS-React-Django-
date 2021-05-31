59d0a550b85f4f55f6d82a9f25d13755bf142d46
SherazLiaqat <sherazliaqat099@gmailcom>
2021-05-31 15:24:57 +0500

    Death+Contact Api done

--- react-2/src/components/Pages/Extrapages/Contact.js
+++ react-2/src/components/Pages/Extrapages/Contact.js
@@ -1,17 +1,47 @@
-import React from 'react'
-import './Contact.css';
-import {  MdEmail,MdLocationOn} from 'react-icons/md';
-import { FiPhoneCall } from 'react-icons/fi';
-import { BiMessageRounded } from 'react-icons/bi';
-import { FaUserAlt } from 'react-icons/fa';
-import { HiOutlineMailOpen} from 'react-icons/hi';
+import React, { useState } from "react";
+import "./Contact.css";
+import { MdEmail, MdLocationOn } from "react-icons/md";
+import { FiPhoneCall } from "react-icons/fi";
+import { BiMessageRounded } from "react-icons/bi";
+import { FaUserAlt } from "react-icons/fa";
+import { HiOutlineMailOpen } from "react-icons/hi";
 
-class Contact extends React.Component {
-  constructor()
+import axios from 'axios';
+
+const Contact = () => {
+  const [username, setuserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  
+
+  const contactinfo = async () => {
+    let formfield = new FormData();
+    formfield.append("name", username);
+    formfield.append("email", email);
+    formfield.append("subject", subject);
+    formfield.append("massage", message);
+  
+   await axios({
+    method: "post",
+    url: "http://127.0.0.1:8000/contact/",
+    data: formfield,
+  }).then((response) => {
+    console.log(response.data);
+    
+  });
+}
+
+  {
+    /* constructor()
   {
    super();
    this.state={
-       username:"",
+    username:"",
+    email:"",
+    subject:"",
+    message:"",
+      /* username:"",
        email:"",
        subject:"",
        message:"",
@@ -21,8 +51,8 @@ class Contact extends React.Component {
        subjecterror:"",
        messagaerror:"",
       
-     }}
-     valid(){
+     
+     valid()
       if(!this.state.username.trim("")&&!this.state.email.includes("@")&& !this.state.subject.trim("") && !this.state.message.trim(""))
       {
         this.setState({usernameerror:" username Required",messageerror:"Enter Message" ,emailerror:"Enter valid email adress",subjecterror:"Subject must be Required" })
@@ -53,94 +83,138 @@ class Contact extends React.Component {
       else{
        return true
      }
-    }
     
     
-     submit(){
+    
+     /*submit(){
        this.setState({usernameerror:"", emailerror:"",subjecterror:"",messageerror:""  })
      
        
        if(this.valid()){
        alert("Form has been submited")}
-     }
-  render(){
+     }*/
+
     return (
-        <div className='main-body'>
-            <h4 className="Header"> HAVE ANY QUERY?</h4>
-            <h1 className="Header2"> Contact Us</h1>
-            <p className='intro'>Please complete the form below to contact us. We will do our best to get back to you as soon as possible!</p>
-            <div className='Contact-Div-Location'>
-            <div className='divlocation'>
-        <h1 className='contactform-label'><MdLocationOn />Locate Us</h1>
-      <p className='div-paragraph'> UNIVERSITY OF GUJRAT SIALKOT CAMPUS</p>
-                 
-      </div>
-      <div className='divlocation'>
-        <h1 className='contactform-label'><FiPhoneCall/>Give us a Call</h1>
-        <p className='div-paragraph'> OFFICE: 052-1454678</p>
-                 
-      </div>
-      <div className='divlocation'>
-        <h1 className='contactform-label'><MdEmail/>Get in  Online</h1>
-        <p className='div-paragraph'> example@gmail.com</p>
-                 
-      </div><br/>
-      </div>
-      <div className='contact-detail'>
-            <div className='form-inputs1'>
-        <label className='form-label1'><FaUserAlt />Your Name*</label>
-        <input
-          className='form-input'
-          placeholder=" Enter User Name..."
-          type='text'
-          name='name'
-          onChange={(event)=>{this.setState({username:event.target.value})}}/>
-         <div>
-          <p>{this.state.usernameerror}</p>
-           </div>      
-      </div>
-      <div className='form-inputs1'>
-        <label className='form-label1'><MdEmail/>Your E-mail</label>
-        <input
-          className='form-input'
-          placeholder=" Enter Your Valid Email..."
-          type='email'
-          name='email'
-          onChange={(event)=>{this.setState({username:event.target.value})}}/>
-         
-          <p>{this.state.emailerror}</p>
-          </div>      
-      </div>
-      <div className='contact-detail-2'>
-      <div className='form-inputs2'>
-        <label className='form-label1'><MdEmail/>Your Subject</label>
-        <input
-          className='form-input'
-          type='text'
-          placeholder=" Enter Subject..."
-
-          name='name'
-          onChange={(event)=>{this.setState({username:event.target.value})}}/>
-         
-          <p>{this.state.subjecterror}</p>
-          </div>        
-     
-      <div className='form-inputs2'>
-        <label className='form-label1'><BiMessageRounded/>Your Message</label>
-        <input
-          className='form-input'
-          placeholder=" Enter Your Message..."
-          type='text'
-          name='name'
-          onChange={(event)=>{this.setState({username:event.target.value})}}/>
-         
-          <p>{this.state.messageerror}</p>
-          </div>      
+      <div className="main-body">
+        <h4 className="Header"> HAVE ANY QUERY?</h4>
+        <h1 className="Header2"> Contact Us</h1>
+        <p className="intro">
+          Please complete the form below to contact us. We will do our best to
+          get back to you as soon as possible!
+        </p>
+        <div className="Contact-Div-Location">
+          <div className="divlocation">
+            <h1 className="contactform-label">
+              <MdLocationOn />
+              Locate Us
+            </h1>
+            <p className="div-paragraph">
+              {" "}
+              UNIVERSITY OF GUJRAT SIALKOT CAMPUS
+            </p>
           </div>
-      <button className='contactButton' onClick={()=>this.submit()}> Contact Us</button> 
-      <p className ='last'>Thank You! For Reaching Out We Will get back to you as soon as Possible.</p><br/>
+          <div className="divlocation">
+            <h1 className="contactform-label">
+              <FiPhoneCall />
+              Give us a Call
+            </h1>
+            <p className="div-paragraph"> OFFICE: 052-1454678</p>
+          </div>
+          <div className="divlocation">
+            <h1 className="contactform-label">
+              <MdEmail />
+              Get in Online
+            </h1>
+            <p className="div-paragraph"> example@gmail.com</p>
+          </div>
+          <br />
         </div>
-    )
-}}
+        <div className="contact-detail">
+          <div className="form-inputs1">
+            <label className="form-label1">
+              <FaUserAlt />
+              Your Name*
+            </label>
+            <input
+              className="form-input"
+              placeholder=" Enter User Name..."
+              type="text"
+              name="name"
+              value={username}
+              onChange={(e) => setuserName(e.target.value)}
+              /* onChange={(event)=>{this.setState({username:event.target.value})}}*/
+            />
+            <div>
+              
+            </div>
+          </div>
+          <div className="form-inputs1">
+            <label className="form-label1">
+              <MdEmail />
+              Your E-mail
+            </label>
+            <input
+              className="form-input"
+              placeholder=" Enter Your Valid Email..."
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
 
-export default Contact
+              /* onChange={(event)=>{this.setState({username:event.target.value})}}*/
+            />
+
+           
+          </div>
+        </div>
+        <div className="contact-detail-2">
+          <div className="form-inputs2">
+            <label className="form-label1">
+              <MdEmail />
+              Your Subject
+            </label>
+            <input
+              className="form-input"
+              type="text"
+              placeholder=" Enter Subject..."
+              name="name"
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+              /* onChange={(event)=>{this.setState({username:event.target.value})}}*/
+            />
+
+            
+          </div>
+
+          <div className="form-inputs2">
+            <label className="form-label1">
+              <BiMessageRounded />
+              Your Message
+            </label>
+            <input
+              className="form-input"
+              placeholder=" Enter Your Message..."
+              type="text"
+              name="name"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              /*  onChange={(event)=>{this.setState({username:event.target.value})}}*/
+            />
+
+            
+          </div>
+        </div>
+        <button className="contactButton" onClick={contactinfo}/* onClick={()=>this.submit()}*/>
+          {" "}
+          Contact Us
+        </button>
+        <p className="last">
+          Thank You! For Reaching Out We Will get back to you as soon as
+          Possible.
+        </p>
+        <br />
+      </div>
+    );
+  }
+};
+export default Contact;
--- react-2/src/components/Pages/Extrapages/Event.js
+++ react-2/src/components/Pages/Extrapages/Event.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Blog from '../../Pages/Blog';
 import Heavyrain from '../Infopages/Heavyrain';
+import axios from 'axios';
 import './Event.css';
+import { PostAddSharp } from '@material-ui/icons';
+
 export const Event = () => {
+const[events,setevent]=useState([])
+useEffect(()=>{
+    axios.get('http://127.0.0.1:8000/Earthquake_Events/')
+    .then(res=>{
+        console.log(res)
+    })
+    .catch(err=>{
+console.log(err)
+    })
+})
     return (
         <div className="div-main">
           
+            
             <div  className='event-container-Blog'>
             < Blog/>
             </div>
